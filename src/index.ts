9251aaebb9b540b5a9f201240d85d6eba77bd29d
Reece Kidd <reecekidd95@gmail.com>
2019-09-14 15:50:41 +0100

    feat: Added additional types to the index file

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,41 @@
+import Activtiy from "./models/Activity";
+import ActivityArray from "./models/ActivityArray";
 import AgendaJob from "./models/AgendaJob";
 import CompleteGroupMemberStreakTask from "./models/CompleteGroupMemberStreakTask";
 import CompleteSoloStreakTask from "./models/CompleteSoloStreakTask";
+import CurrentStreak from "./models/CurrentStreak";
 import Feedback from "./models/Feedback";
 import Friend from "./models/Friend";
 import GroupMember from "./models/GroupMember";
 import GroupMemberStreak from "./models/GroupMemberStreak";
 import GroupStreak from "./models/GroupStreak";
+import PastStreak from "./models/PastStreak";
+import PastStreakArray from "./models/PastStreakArray";
+import PopulatedGroupStreak from "./models/PopulatedGroupStreak";
+import PopulatedMember from "./models/PopulatedMember";
 import SoloStreak from "./models/SoloStreak";
 import StreakTrackingEvent from "./models/StreakTrackingEvent";
 import User from "./models/User";
+
 import { streakoidFactory } from "./streakoid";
 
 export {
   streakoidFactory,
+  Activtiy,
+  ActivityArray,
   AgendaJob,
   CompleteGroupMemberStreakTask,
   CompleteSoloStreakTask,
+  CurrentStreak,
   Feedback,
   Friend,
   GroupMember,
   GroupMemberStreak,
   GroupStreak,
+  PastStreak,
+  PastStreakArray,
+  PopulatedGroupStreak,
+  PopulatedMember,
   SoloStreak,
   StreakTrackingEvent,
   User
