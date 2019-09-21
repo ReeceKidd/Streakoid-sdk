734c9b0693b642dcec8532c33e3fba230bc47a73
Reece Kidd <reecekidd95@gmail.com>
2019-09-21 18:39:19 +0700

    feat: Updated streakoid client to return client without timezone

--- src/streakoidClient.ts
+++ src/streakoidClient.ts
@@ -3,12 +3,21 @@ import SupportedRequestHeaders from "./SupportedRequestHeaders";
 
 export const streakoidClientFactory = (
   applicationUrl: string,
-  timezone: string
+  timezone?: string
 ) => {
+  if (timezone) {
+    return axios.create({
+      headers: {
+        "Content-Type": "application/json",
+        [SupportedRequestHeaders.xTimezone]: timezone
+      },
+      baseURL: applicationUrl
+    });
+  }
+
   return axios.create({
     headers: {
-      "Content-Type": "application/json",
-      [SupportedRequestHeaders.xTimezone]: timezone
+      "Content-Type": "application/json"
     },
     baseURL: applicationUrl
   });
