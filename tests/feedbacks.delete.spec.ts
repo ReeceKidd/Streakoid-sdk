89e1fbd5f8e1039a7bab6c597e08ba81d004761b
Reece Kidd <reecekidd95@gmail.com>
2019-10-06 13:28:37 +0100

    feat: Updated beforeAll hook in feedback.delete unit test

--- tests/feedbacks.delete.spec.ts
+++ tests/feedbacks.delete.spec.ts
@@ -6,42 +6,39 @@ const username = "delete-feedback-user";
 jest.setTimeout(120000);
 
 describe("DELETE /feedbacks/:feedbackId", () => {
-    let registeredUserId: string;
+  let registeredUserId: string;
 
-    beforeAll(async () => {
-        const registrationResponse = await streakoid.users.create(
-            {
-                email,
-                username
-            }
-        );
-        registeredUserId = registrationResponse._id;
+  beforeAll(async () => {
+    const user = await streakoid.users.create({
+      email,
+      username
+    });
+    registeredUserId = user._id;
+  });
+
+  afterAll(async () => {
+    await streakoid.users.deleteOne(registeredUserId);
+  });
+
+  test(`deletes feedback`, async () => {
+    expect.assertions(1);
+
+    const feedbackPageUrl = "/solo-streaks";
+    const feedbackUsername = "username";
+    const feedbackUserEmail = "userEmail";
+    const feedbackText = "feedback";
+
+    const feedback = await streakoid.feedbacks.create({
+      userId: registeredUserId,
+      pageUrl: feedbackPageUrl,
+      username: feedbackUsername,
+      userEmail: feedbackUserEmail,
+      feedbackText
     });
 
-    afterAll(async () => {
-        await streakoid.users.deleteOne(registeredUserId);
-    });
+    const { _id } = feedback;
 
-    test(`deletes feedback`, async () => {
-        expect.assertions(1);
-
-        const feedbackPageUrl = "/solo-streaks";
-        const feedbackUsername = "username";
-        const feedbackUserEmail = "userEmail";
-        const feedbackText = "feedback";
-
-        const feedback = await streakoid.feedbacks.create({
-            userId: registeredUserId,
-            pageUrl: feedbackPageUrl,
-            username: feedbackUsername,
-            userEmail: feedbackUserEmail,
-            feedbackText
-        }
-        );
-
-        const { _id } = feedback;
-
-        const response = await streakoid.feedbacks.deleteOne(_id);
-        expect(response.status).toEqual(204);
-    });
+    const response = await streakoid.feedbacks.deleteOne(_id);
+    expect(response.status).toEqual(204);
+  });
 });
