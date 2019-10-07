95f80b8b1a7edc9799fefb8cfbc1877d70469c39
Reece Kidd <reecekidd95@gmail.com>
2019-10-07 09:31:09 +0100

    feat: Removed activity from solo streaks

--- src/models/SoloStreak.ts
+++ src/models/SoloStreak.ts
@@ -1,6 +1,5 @@
 import CurrentStreak from './CurrentStreak';
 import StreakStatus from '../StreakStatus';
-import Activity from './Activity';
 import PastStreak from './PastStreak';
 
 interface SoloStreak {
@@ -10,7 +9,6 @@ interface SoloStreak {
     status: StreakStatus;
     completedToday: boolean;
     active: boolean;
-    activity: Array<Activity>;
     currentStreak: CurrentStreak;
     pastStreaks: Array<PastStreak>;
     timezone: string;
