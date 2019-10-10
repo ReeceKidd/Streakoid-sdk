b429494e279fdbfa6e184c36712b7a7d7d6f658d
Reece Kidd <reecekidd95@gmail.com>
2019-10-10 07:26:03 +0100

    feat: Made teamStreakId option on CompleteGroupMemberStreakTask

--- src/models/CompleteGroupMemberStreakTask.ts
+++ src/models/CompleteGroupMemberStreakTask.ts
@@ -4,13 +4,13 @@ interface CompleteGroupMemberStreakTask {
     _id: string;
     streakId: string;
     userId: string;
-    teamStreakId: string;
     groupMemberStreakId: string;
     groupStreakType: GroupStreakTypes;
     taskCompleteTime: Date;
     taskCompleteDay: string;
     createdAt: string;
     updatedAt: string;
+    teamStreakId?: string;
 }
 
 export default CompleteGroupMemberStreakTask;
