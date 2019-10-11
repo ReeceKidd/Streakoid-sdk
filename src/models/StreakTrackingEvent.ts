6df478eb356ce30b94b3a54d77af9ae67af775bd
Reece Kidd <reecekidd95@gmail.com>
2019-10-11 09:23:46 +0100

    feat: Updated streak tracking event model

--- src/models/StreakTrackingEvent.ts
+++ src/models/StreakTrackingEvent.ts
@@ -1,10 +1,15 @@
+import StreakTypes from '../streakTypes';
+import GroupStreakTypes from '../GroupStreakTypes';
+
 interface StreakTrackingEvent {
     _id: string;
     type: StreakTrackingEvent;
     streakId: string;
+    streakType: StreakTypes;
     userId: string;
     createdAt: string;
     updatedAt: string;
+    groupStreakType: GroupStreakTypes;
 }
 
 export default StreakTrackingEvent;
