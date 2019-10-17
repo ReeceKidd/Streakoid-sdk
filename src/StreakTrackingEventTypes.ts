01ef67ef75c8c078ee255a7b6f7f3f7bb974c60d
Reece Kidd <reecekidd95@gmail.com>
2019-10-17 08:02:26 +0100

    feat: Added a forcedLostStreakBecauseTeamMemberDidNotCompleteTask event to StreakTrackingEventTypes

--- src/StreakTrackingEventTypes.ts
+++ src/StreakTrackingEventTypes.ts
@@ -2,6 +2,7 @@ enum StreakTrackingEventTypes {
     lostStreak = 'lostStreak',
     maintainedStreak = 'maintainedStreak',
     inactiveStreak = 'inactiveStreak',
+    forcedLostStreakBecauseTeamMemberDidNotCompleteTask = 'forcedLostStreakBecauseTeamMemberDidNotCompleteTask',
 }
 
 export default StreakTrackingEventTypes;
