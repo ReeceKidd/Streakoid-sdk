5c187c61f29905d5109cdf670a7be3d973de1bb4
Reece Kidd <reecekidd95@gmail.com>
2019-11-09 15:40:15 +0000

    feat: Corrected SDK Urls

--- tests/setup/environment.ts
+++ tests/setup/environment.ts
@@ -1,6 +1,6 @@
-export const databaseUri = 'mongodb://localhost:27017/streakoid-test';
+export const databaseUri = 'mongodb+srv://reece:G3PUluuoctP4wBfJ@cluster0-kxrys.mongodb.net/PROD';
 export const username = 'tester';
 export const email = 'tester@streakoid.com';
 export const password = 'Password1';
-export const applicationUrl = 'http://localhost:3001';
+export const applicationUrl = 'https://qz6l18dx09.execute-api.eu-west-1.amazonaws.com/dev';
 export const originalImageUrl = 'https://streakoid-profile-pictures.s3-eu-west-1.amazonaws.com/steve.jpg';
