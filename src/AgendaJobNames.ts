508f253ade65cd8061923d4120a802141a69c312
Reece Kidd <reecekidd95@gmail.com>
2019-11-10 16:55:20 +0000

    feat: Added complete streaks reminder to AgendaJobNames

--- src/AgendaJobNames.ts
+++ src/AgendaJobNames.ts
@@ -2,6 +2,7 @@ export enum AgendaJobNames {
     soloStreakDailyTracker = 'soloStreakDailyTracker',
     teamStreakDailyTracker = 'teamStreakDailyTracker',
     adjustForDaylightSavingsTime = 'adjustForDaylightSavingsTime',
+    completeStreaksReminder = 'completeStreaksReminder',
 }
 
 export default AgendaJobNames;
