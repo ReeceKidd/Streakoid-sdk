d9a6ffd372d8c19aaad2208b2011278d99c27095
Reece Kidd <reecekidd95@gmail.com>
2019-11-19 14:35:09 +0000

    feat: Upgraded structure of badge

--- src/models/Badge.ts
+++ src/models/Badge.ts
@@ -1,10 +1,8 @@
 interface Badge {
     _id: string;
     name: string;
-    currentLevel: number;
     description: string;
-    levels: Array<{ level: number; criteria: string }>;
-    color: string;
+    levels: Array<{ level: number; criteria: string; color: string }>;
     icon: string;
 }
 
