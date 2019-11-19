655b4f1c014182904c3f21a3bc4eba964acb1453
Reece Kidd <reecekidd95@gmail.com>
2019-11-19 13:01:45 +0000

    feat: Added parentBadge and nextLevel badge

--- src/models/Badge.ts
+++ src/models/Badge.ts
@@ -1,9 +1,11 @@
 interface Badge {
     _id: string;
+    parentBadge: Badge;
     name: string;
     description: string;
     color: string;
     icon: string;
+    nextLevel?: Badge;
 }
 
 export default Badge;
