f60413d43c5be4851764a7874f4a244ded1afd70
Reece Kidd <reecekidd95@gmail.com>
2019-11-23 16:54:47 +0000

    feat: Updated agenda job names to include challengeStreakDailyTracker

--- src/AgendaJobNames.ts
+++ src/AgendaJobNames.ts
@@ -1,6 +1,7 @@
 export enum AgendaJobNames {
     soloStreakDailyTracker = 'soloStreakDailyTracker',
     teamStreakDailyTracker = 'teamStreakDailyTracker',
+    challengeStreakDailyTracker = 'challengeStreakDailyTracker',
     adjustForDaylightSavingsTime = 'adjustForDaylightSavingsTime',
     completeStreaksReminder = 'completeStreaksReminder',
 }
