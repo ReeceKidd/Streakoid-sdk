1caf0fdaac9fbe1285b74502b36056c9cf5d4aee
Reece Kidd <reecekidd95@gmail.com>
2019-11-24 10:10:58 +0000

    feat: Removed levels from badge SDK

--- src/models/Badge.ts
+++ src/models/Badge.ts
@@ -2,7 +2,6 @@ interface Badge {
     _id: string;
     name: string;
     description: string;
-    levels: Array<{ _id: string; level: number; criteria: string; color: string }>;
     icon: string;
     createdAt: string;
     updatedAt: string;
