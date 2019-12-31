202898b07d81898cbd5268902260c8a783e6f307
Reece Kidd <reecekidd95@gmail.com>
2019-12-31 10:42:46 +0000

    chore: Added notes.create intergration test

---
+++ tests/notes.create.spec.ts
@@ -0,0 +1,56 @@
+import { StreakoidFactory } from '../src/streakoid';
+import { streakoidTest } from './setup/streakoidTest';
+import { getPayingUser } from './setup/getPayingUser';
+import { isTestEnvironment } from './setup/isTestEnvironment';
+import { setUpDatabase } from './setup/setUpDatabase';
+import { tearDownDatabase } from './setup/tearDownDatabase';
+
+jest.setTimeout(120000);
+
+const streakName = 'Daily Spanish';
+const streakDescription = 'I must do 30 minutes of Spanish everyday';
+const numberOfMinutes = 30;
+
+describe('POST /notes', () => {
+    let streakoid: StreakoidFactory;
+    let userId: string;
+
+    beforeAll(async () => {
+        if (isTestEnvironment()) {
+            await setUpDatabase();
+            const user = await getPayingUser();
+            userId = user._id;
+            streakoid = await streakoidTest();
+        }
+    });
+
+    afterAll(async () => {
+        if (isTestEnvironment()) {
+            await tearDownDatabase();
+        }
+    });
+
+    test(`creates a note for a solo streak`, async () => {
+        expect.assertions(14);
+
+        const soloStreak = await streakoid.soloStreaks.create({
+            userId,
+            streakName,
+            streakDescription,
+            numberOfMinutes,
+        });
+
+        const noteText = 'Finished reading book';
+
+        const note = await streakoid.notes.create({ userId, streakId: soloStreak._id, note: noteText });
+
+        expect(note.userId).toEqual(userId);
+        expect(note.streakId).toEqual(soloStreak._id);
+        expect(note.note).toEqual(noteText);
+        expect(note.createdAt).toEqual(expect.any(String));
+        expect(note.updatedAt).toEqual(expect.any(String));
+        expect(Object.keys(note).sort()).toEqual(
+            ['_id', 'userId', 'streakId', 'note', 'createdAt', 'updatedAt', '__v'].sort(),
+        );
+    });
+});
