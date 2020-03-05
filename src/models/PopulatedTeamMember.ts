2b8a0eff9d6f45321f5875b1b72b83204284382e
Reece Kidd <reecekidd95@gmail.com>
2020-03-05 16:26:48 -0300

    feat: Updated PopulatedTeamMember to include currentStreak, longestStreak and joinedStreak

--- src/models/PopulatedTeamMember.ts
+++ src/models/PopulatedTeamMember.ts
@@ -1,10 +1,12 @@
-import TeamMemberStreak from './TeamMemberStreak';
+import { CurrentStreak } from '..';
 
 interface PopulatedTeamMember {
     _id: string;
     username: string;
     profileImage: string;
-    teamMemberStreak: TeamMemberStreak;
+    currentStreak: CurrentStreak;
+    longestStreak: number;
+    joinedStreak: Date;
 }
 
 export default PopulatedTeamMember;
