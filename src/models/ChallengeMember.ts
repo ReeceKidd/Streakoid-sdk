d8c068baa06d9d84c2d5884d7ee7cb2d7980fec0
Reece Kidd <reecekidd95@gmail.com>
2020-03-05 11:14:22 -0300

    feat: Updated challenge member to include currentStreak of member and longestPastStreak

--- src/models/ChallengeMember.ts
+++ src/models/ChallengeMember.ts
@@ -1,7 +1,11 @@
+import CurrentStreak from './CurrentStreak';
+
 interface ChallengeMember {
     username: string;
     userId: string;
     profileImage: string;
+    currentStreak: CurrentStreak;
+    longestPastStreak: number;
 }
 
 export default ChallengeMember;
