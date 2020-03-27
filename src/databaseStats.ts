3af010877717b99f1fc07a14215264960644ef12
Reece Kidd <reecekidd95@gmail.com>
2020-03-27 07:15:45 -0300

    refactor: Converted stats to database stats

--- src/databaseStats.ts
+++ src/databaseStats.ts
@@ -2,11 +2,11 @@ import { AxiosInstance } from 'axios';
 
 import ApiVersions from './ApiVersions';
 import RouterCategories from './RouterCategories';
-import Stats from './models/Stats';
+import DatabaseStats from './models/DatabaseStats';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const databaseStats = (streakoidClient: AxiosInstance) => {
-    const get = async (): Promise<Stats> => {
+    const get = async (): Promise<DatabaseStats> => {
         try {
             const { data } = await streakoidClient.get(`/${ApiVersions.v1}/${RouterCategories.databaseStats}`);
             return data;
