87829fbcf0922fd30c6b72f125c8fa95e70f019d
Reece Kidd <reecekidd95@gmail.com>
2020-04-14 15:31:05 +0100

    feat: Made completeAllStreaksReminder optional on UserPushNotifications

--- src/models/UserPushNotifications.ts
+++ src/models/UserPushNotifications.ts
@@ -10,7 +10,7 @@ interface UserPushNotifications {
     badgeUpdates: {
         enabled: boolean;
     };
-    completeAllStreaksReminder: CompleteAllStreaksReminder;
+    completeAllStreaksReminder?: CompleteAllStreaksReminder;
 }
 
 export default UserPushNotifications;
