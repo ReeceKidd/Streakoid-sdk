37983a0264c5c7cd7e645368e541c3e4cb014d9a
Reece Kidd <reecekidd95@gmail.com>
2020-05-29 13:01:00 +0100

    refactor: Attempted to fix streakoidAxiosSDKFactory

--- src/streakoidAxiosSDKFactory.ts
+++ src/streakoidAxiosSDKFactory.ts
@@ -3,15 +3,23 @@ import { GetRequest, PostRequest, DeleteRequest, PatchRequest } from './request'
 import { StreakoidSDK, streakoidSDKFactory } from './streakoidSDKFactory';
 
 export const streakoidAxiosSDKFactory = (streakoidClient: AxiosInstance): StreakoidSDK => {
-    const getRequest: GetRequest = ({ route }) => streakoidClient.get(route);
-    const getRequestActivityFeed: GetRequest = ({ route }) => streakoidClient.get(route);
-    const postRequest: PostRequest = ({ route, params }) => streakoidClient.post(route, params);
-    const patchRequest: PatchRequest = ({ route, params }) => {
-        if (params) {
-            return streakoidClient.patch(route, params);
-        }
-        return streakoidClient.patch(route);
+    const getRequest: GetRequest = async ({ route }) => {
+        const { body } = await streakoidClient.get(route);
+        return body;
     };
-    const deleteRequest: DeleteRequest = ({ route }) => streakoidClient.delete(route);
+    const getRequestActivityFeed: GetRequest = ({ route }) => streakoidClient.get(route);
+    const postRequest: PostRequest = async ({ route, params }) => {
+        const { body } = await streakoidClient.post(route, params);
+        return body;
+    };
+    const patchRequest: PatchRequest = async ({ route, params }) => {
+        if (params) {
+            const { body } = await streakoidClient.patch(route, params);
+            return body;
+        }
+        const { body } = await streakoidClient.patch(route);
+        return body;
+    };
+    const deleteRequest: DeleteRequest = async ({ route }) => streakoidClient.delete(route);
     return streakoidSDKFactory({ getRequest, getRequestActivityFeed, postRequest, patchRequest, deleteRequest });
 };
