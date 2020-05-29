4be47d5cff8ee332f43ca1d2b096e5f2ac8017c3
Reece Kidd <reecekidd95@gmail.com>
2020-05-29 10:59:48 +0100

    chore: Cleared up package.json to remove intergration tests

--- jest.intergration.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    testEnvironment: 'node',
-    modulePathIgnorePatterns: ['./coverage/*', './jest.config.js', './lib/*', './src/*'],
-    testMatch: ['**/tests/**/*.spec.(ts|js)'],
-};
