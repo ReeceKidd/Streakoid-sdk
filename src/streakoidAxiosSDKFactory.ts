e7fdb3aad273df958c0911b3e9cd540cbfa035a7
Reece Kidd <reecekidd95@gmail.com>
2020-05-29 13:02:08 +0100

    refactor: Changed from body to data

--- src/streakoidAxiosSDKFactory.ts
+++ src/streakoidAxiosSDKFactory.ts
@@ -4,21 +4,21 @@ import { StreakoidSDK, streakoidSDKFactory } from './streakoidSDKFactory';
 
 export const streakoidAxiosSDKFactory = (streakoidClient: AxiosInstance): StreakoidSDK => {
     const getRequest: GetRequest = async ({ route }) => {
-        const { body } = await streakoidClient.get(route);
-        return body;
+        const { data } = await streakoidClient.get(route);
+        return data;
     };
     const getRequestActivityFeed: GetRequest = ({ route }) => streakoidClient.get(route);
     const postRequest: PostRequest = async ({ route, params }) => {
-        const { body } = await streakoidClient.post(route, params);
-        return body;
+        const { data } = await streakoidClient.post(route, params);
+        return data;
     };
     const patchRequest: PatchRequest = async ({ route, params }) => {
         if (params) {
-            const { body } = await streakoidClient.patch(route, params);
-            return body;
+            const { data } = await streakoidClient.patch(route, params);
+            return data;
         }
-        const { body } = await streakoidClient.patch(route);
-        return body;
+        const { data } = await streakoidClient.patch(route);
+        return data;
     };
     const deleteRequest: DeleteRequest = async ({ route }) => streakoidClient.delete(route);
     return streakoidSDKFactory({ getRequest, getRequestActivityFeed, postRequest, patchRequest, deleteRequest });
