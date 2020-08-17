e30619933b0b1ded7edd935e2001da2c1fe9408a
Reece Kidd <reecekidd95@gmail.com>
2020-08-17 16:02:02 +0100

    refactor: Moved get user solo, challenge, team member and team streaks to the user object

--- src/user.spec.ts
+++ src/user.spec.ts
@@ -2,6 +2,9 @@ import { user as userImport } from './user';
 import UserTypes from '@streakoid/streakoid-models/lib/Types/UserTypes';
 import StreakStatus from '@streakoid/streakoid-models/lib/Types/StreakStatus';
 import { GetAllSoloStreaksSortFields } from './soloStreaks';
+import { GetAllChallengeStreaksSortFields } from './challengeStreaks';
+import { GetAllTeamMemberStreaksSortFields } from './teamMemberStreaks';
+import { GetAllTeamStreaksSortFields } from './teamStreaks';
 
 describe('SDK users', () => {
     const getRequest = jest.fn().mockResolvedValue(true);
@@ -115,6 +118,257 @@ describe('SDK users', () => {
             await user.soloStreaks({ limit });
 
             expect(getRequest).toBeCalledWith({ route: `/v1/user/solo-streaks?limit=${limit}&` });
+        });
+    });
+
+    describe('challengeStreaks', () => {
+        test('calls GET with correct URL when no query parameters are passed', async () => {
+            expect.assertions(1);
+
+            await user.challengeStreaks({});
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/challenge-streaks?` });
+        });
+
+        test('calls GET with correct URL when completedToday query parameters is passed', async () => {
+            expect.assertions(1);
+
+            const completedToday = true;
+
+            await user.challengeStreaks({ completedToday });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/challenge-streaks?completedToday=true&` });
+        });
+
+        test('calls GET with correct URL when timezone query parameters is passed', async () => {
+            expect.assertions(1);
+
+            const timezone = `Europe/London`;
+
+            await user.challengeStreaks({ timezone });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/challenge-streaks?timezone=${timezone}&` });
+        });
+
+        test('calls GET with correct URL when active query parameters is passed', async () => {
+            expect.assertions(1);
+
+            const active = true;
+
+            await user.challengeStreaks({ active });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/challenge-streaks?active=${active}&` });
+        });
+
+        test('calls GET with correct URL when status query parameters is passed', async () => {
+            expect.assertions(1);
+
+            const status = StreakStatus.live;
+
+            await user.challengeStreaks({ status });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/challenge-streaks?status=${status}&` });
+        });
+
+        test('calls GET with correct URL when sortField query parameters is passed', async () => {
+            expect.assertions(1);
+
+            const sortField = GetAllChallengeStreaksSortFields.currentStreak;
+
+            await user.challengeStreaks({ sortField });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/challenge-streaks?sortField=${sortField}&` });
+        });
+
+        test('calls GET with correct URL when limit query parameters is passed', async () => {
+            expect.assertions(1);
+
+            const limit = 20;
+
+            await user.challengeStreaks({ limit });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/challenge-streaks?limit=${limit}&` });
+        });
+    });
+
+    describe('teamMemberStreaks', () => {
+        const userId = 'userId';
+        const teamStreakId = 'teamStreakId';
+        const completedToday = true;
+        const timezone = 'Europe/London';
+        const active = true;
+        const sortField = GetAllTeamMemberStreaksSortFields.currentStreak;
+        const limit = 20;
+
+        const query = {
+            userId,
+            teamStreakId,
+            completedToday,
+            timezone,
+            active,
+            sortField,
+            limit,
+        };
+
+        test('calls GET with correct URL when no query parameters are passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({});
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-member-streaks?` });
+        });
+
+        test('calls GET with correct URL when userId query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({ userId });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-member-streaks?userId=${userId}&` });
+        });
+
+        test('calls GET with correct URL when teamStreakId query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({ teamStreakId });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-member-streaks?teamStreakId=${teamStreakId}&` });
+        });
+
+        test('calls GET with correct URL when completedToday query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({ completedToday });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-member-streaks?completedToday=true&` });
+        });
+
+        test('calls GET with correct URL when timezone query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({ timezone });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-member-streaks?timezone=${timezone}&` });
+        });
+
+        test('calls GET with correct URL when active query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({ active });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-member-streaks?active=${active}&` });
+        });
+
+        test('calls GET with correct URL when active sort paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({ sortField });
+
+            expect(getRequest).toBeCalledWith({
+                route: `/v1/user/team-member-streaks?sortField=${sortField}&`,
+            });
+        });
+
+        test('calls GET with correct URL when limit paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks({ limit });
+
+            expect(getRequest).toBeCalledWith({
+                route: `/v1/user/team-member-streaks?limit=${limit}&`,
+            });
+        });
+
+        test('calls GET with correct URL when all query parameters are passed', async () => {
+            expect.assertions(1);
+
+            await user.teamMemberStreaks(query);
+
+            expect(getRequest).toBeCalledWith({
+                route: `/v1/user/team-member-streaks?userId=${userId}&teamStreakId=${teamStreakId}&completedToday=${completedToday}&timezone=${timezone}&active=${active}&sortField=${sortField}&limit=${limit}&`,
+            });
+        });
+    });
+
+    describe('teamStreaks', () => {
+        const timezone = 'Europe/London';
+        const status = StreakStatus.live;
+        const completedToday = true;
+        const active = true;
+        const sortField = GetAllTeamStreaksSortFields.currentStreak;
+        const limit = 20;
+
+        const query = {
+            timezone,
+            status,
+            completedToday,
+            active,
+            sortField,
+            limit,
+        };
+        test('calls GET with correct URL when no query parameters are passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks({});
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-streaks?` });
+        });
+
+        test('calls GET with correct URL when timezone query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks({ timezone });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-streaks?timezone=${timezone}&` });
+        });
+
+        test('calls GET with correct URL when status query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks({ status });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-streaks?status=${status}&` });
+        });
+
+        test('calls GET with correct URL when completedToday query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks({ completedToday });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-streaks?completedToday=${completedToday}&` });
+        });
+
+        test('calls GET with correct URL when active query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks({ active });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-streaks?active=${active}&` });
+        });
+
+        test('calls GET with correct URL when sortField query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks({ sortField });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-streaks?sortField=${sortField}&` });
+        });
+
+        test('calls GET with correct URL when limit query paramter is passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks({ limit });
+
+            expect(getRequest).toBeCalledWith({ route: `/v1/user/team-streaks?limit=${limit}&` });
+        });
+
+        test('calls GET with correct URL when all parameters are passed', async () => {
+            expect.assertions(1);
+
+            await user.teamStreaks(query);
+
+            expect(getRequest).toBeCalledWith({
+                route: `/v1/user/team-streaks?timezone=${timezone}&status=${status}&completedToday=${completedToday}&active=${true}&sortField=${sortField}&limit=${limit}&`,
+            });
         });
     });
 });
--- src/user.ts
+++ src/user.ts
@@ -8,6 +8,11 @@ import UserTypes from '@streakoid/streakoid-models/lib/Types/UserTypes';
 import StreakStatus from '@streakoid/streakoid-models/lib/Types/StreakStatus';
 import { GetAllSoloStreaksSortFields } from './soloStreaks';
 import { SoloStreak } from '@streakoid/streakoid-models/lib/Models/SoloStreak';
+import { GetAllChallengeStreaksSortFields } from './challengeStreaks';
+import { GetAllTeamMemberStreaksSortFields } from './teamMemberStreaks';
+import { GetAllTeamStreaksSortFields } from './teamStreaks';
+import { TeamMemberStreak } from '@streakoid/streakoid-models/lib/Models/TeamMemberStreak';
+import { PopulatedTeamStreak } from '@streakoid/streakoid-models/lib/Models/PopulatedTeamStreak';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const user = ({ getRequest, patchRequest }: { getRequest: GetRequest; patchRequest: PatchRequest }) => {
@@ -99,11 +104,159 @@ const user = ({ getRequest, patchRequest }: { getRequest: GetRequest; patchReque
         }
     };
 
+    const challengeStreaks = async ({
+        completedToday,
+        timezone,
+        active,
+        status,
+        sortField,
+        limit,
+    }: {
+        timezone?: string;
+        status?: StreakStatus;
+        active?: boolean;
+        completedToday?: boolean;
+        sortField?: GetAllChallengeStreaksSortFields;
+        limit?: number;
+    }): Promise<SoloStreak[]> => {
+        try {
+            let getAllChallengeStreaksURL = `/${ApiVersions.v1}/${RouterCategories.user}/${RouterCategories.challengeStreaks}?`;
+
+            if (timezone) {
+                getAllChallengeStreaksURL = `${getAllChallengeStreaksURL}timezone=${timezone}&`;
+            }
+
+            if (status) {
+                getAllChallengeStreaksURL = `${getAllChallengeStreaksURL}status=${status}&`;
+            }
+
+            if (completedToday !== undefined) {
+                getAllChallengeStreaksURL = `${getAllChallengeStreaksURL}completedToday=${Boolean(completedToday)}&`;
+            }
+
+            if (active !== undefined) {
+                getAllChallengeStreaksURL = `${getAllChallengeStreaksURL}active=${Boolean(active)}&`;
+            }
+
+            if (sortField) {
+                getAllChallengeStreaksURL = `${getAllChallengeStreaksURL}sortField=${sortField}&`;
+            }
+
+            if (limit) {
+                getAllChallengeStreaksURL = `${getAllChallengeStreaksURL}limit=${limit}&`;
+            }
+
+            return getRequest({ route: getAllChallengeStreaksURL });
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    };
+
+    const teamMemberStreaks = async ({
+        userId,
+        teamStreakId,
+        completedToday,
+        timezone,
+        active,
+        sortField,
+        limit,
+    }: {
+        userId?: string;
+        teamStreakId?: string;
+        completedToday?: boolean;
+        timezone?: string;
+        active?: boolean;
+        sortField?: GetAllTeamMemberStreaksSortFields;
+        limit?: number;
+    }): Promise<TeamMemberStreak[]> => {
+        try {
+            let getAllTeamMemberStreaksURL = `/${ApiVersions.v1}/${RouterCategories.user}/${RouterCategories.teamMemberStreaks}?`;
+
+            if (userId) {
+                getAllTeamMemberStreaksURL = `${getAllTeamMemberStreaksURL}userId=${userId}&`;
+            }
+
+            if (teamStreakId) {
+                getAllTeamMemberStreaksURL = `${getAllTeamMemberStreaksURL}teamStreakId=${teamStreakId}&`;
+            }
+
+            if (completedToday !== undefined) {
+                getAllTeamMemberStreaksURL = `${getAllTeamMemberStreaksURL}completedToday=${Boolean(completedToday)}&`;
+            }
+
+            if (timezone) {
+                getAllTeamMemberStreaksURL = `${getAllTeamMemberStreaksURL}timezone=${timezone}&`;
+            }
+
+            if (active !== undefined) {
+                getAllTeamMemberStreaksURL = `${getAllTeamMemberStreaksURL}active=${Boolean(active)}&`;
+            }
+            if (sortField) {
+                getAllTeamMemberStreaksURL = `${getAllTeamMemberStreaksURL}sortField=${sortField}&`;
+            }
+            if (limit) {
+                getAllTeamMemberStreaksURL = `${getAllTeamMemberStreaksURL}limit=${limit}&`;
+            }
+
+            return getRequest({ route: getAllTeamMemberStreaksURL });
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    };
+
+    const teamStreaks = async ({
+        timezone,
+        status,
+        completedToday,
+        active,
+        sortField,
+        limit,
+    }: {
+        creatorId?: string;
+        memberId?: string;
+        timezone?: string;
+        status?: StreakStatus;
+        completedToday?: boolean;
+        active?: boolean;
+        sortField?: GetAllTeamStreaksSortFields;
+        limit?: number;
+    }): Promise<PopulatedTeamStreak[]> => {
+        try {
+            let getAllTeamStreaksURL = `/${ApiVersions.v1}/${RouterCategories.user}/${RouterCategories.teamStreaks}?`;
+
+            if (timezone) {
+                getAllTeamStreaksURL = `${getAllTeamStreaksURL}timezone=${timezone}&`;
+            }
+            if (status) {
+                getAllTeamStreaksURL = `${getAllTeamStreaksURL}status=${status}&`;
+            }
+            if (completedToday !== undefined) {
+                getAllTeamStreaksURL = `${getAllTeamStreaksURL}completedToday=${Boolean(completedToday)}&`;
+            }
+            if (active !== undefined) {
+                getAllTeamStreaksURL = `${getAllTeamStreaksURL}active=${Boolean(active)}&`;
+            }
+            if (sortField) {
+                getAllTeamStreaksURL = `${getAllTeamStreaksURL}sortField=${sortField}&`;
+            }
+            if (limit) {
+                getAllTeamStreaksURL = `${getAllTeamStreaksURL}limit=${limit}&`;
+            }
+
+            return getRequest({ route: getAllTeamStreaksURL });
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    };
+
     return {
         getCurrentUser,
         updateCurrentUser,
         pushNotifications: pushNotifications({ patchRequest }),
         soloStreaks,
+        challengeStreaks,
+        teamMemberStreaks,
+        teamStreaks,
     };
 };
 
